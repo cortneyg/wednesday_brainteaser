ff04396b004d17c0a81a888c4e82392b14ac7287
Cortney Gardner <cortneygardner@PC-LM31571.overstock.com>
2018-10-03 18:44:30 -0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import List from './List';
+import TodoForm from './TodoForm';
 
 class App extends Component {
+  state = { todos: [] }
+
+  getUniqId = () => {
+   //NOTE We are just using this as a helper function for id's since we aren't using a db yet
+   return Math.floor((1 + Math.random()) * 0x10000)
+     .toString(16)
+     .substring(1);
+  }
+
+  addItem = (name) => {
+    const { todos } = this.state;
+    const todo = { name, id: this.getUniqId() , complete: false }
+    this.setState({ todos: [todo, ...todos] }); 
+  }
+
   render() {
+    const { todos } = this.state;
+ 
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <TodoForm addItem={this.addItem} />
+        <List name="Todo List" items={todos} />
       </div>
     );
   }
---
+++ src/component/TodoList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Todo from './Todo'
+
+const List = ({ items, name }) => (
+  <div>
+    <h2>{name}</h2>
+    <ul> 
+     { items.map( item => <Todo key={item.id} id={item.id} name={item.name} complete={item.complete} /> )}
+    </ul>
+  </div>
+)
+
+
+export default TodoList;
